f8761eb6e544dded7bbe82eba2be9ca0317ff739
Gordon <js0955892814@gmail.com>
2021-01-17 16:11:37 +0800

    addcode

--- application.py
+++ application.py
@@ -54,10 +54,10 @@ HANDLER = WebhookHandler(LINE_SECRET)
 # IMGUR_CLIENT = Imgur(config=IMGUR_CONFIG)
 
 
-@app.route("/")
-def hello():
-    "hello world"
-    return "Hello World!!!!!"
+# @app.route("/")
+# def hello():
+#     "hello world"
+#     return "Hello World!!!!!"
 
 
 # def azure_describe(url):
@@ -249,7 +249,20 @@ def callback():
         print("Check the channel secret/access token.")
         abort(400)
     return "OK"
-
+    
+@HANDLER.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    url_dict = {
+      "TIBAME":"https://www.tibame.com/coursegoodjob/traffic_cli", 
+      "HELP":"https://developers.line.biz/zh-hant/docs/messaging-api/"}
+# 將要發出去的文字變成TextSendMessage
+    try:
+        url = url_dict[event.message.text.upper()]
+        message = TextSendMessage(text=url)
+    except:
+        message = TextSendMessage(text=event.message.text)
+# 回覆訊息
+    LINE_BOT.reply_message(event.reply_token, message)
 
 
 
